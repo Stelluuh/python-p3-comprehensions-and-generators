5a61c077706fa1b1bb3b161634811f111d941b5b
stella <sftamesis13@gmail.com>
2024-06-03 15:48:05 -0700

    add psuedocode

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    evenNumbers = [num for num in num_list if num % 2 == 0]
-    return evenNumbers
+    return [num for num in num_list if num % 2 == 0]
+    # Loop through each number in num_list. For each iteration, the current element of 'num_list' is assigned to the variable num.
+    #condition is to check if each number is even by seeing if the num divided by 2 is zero. if it is, then that number is even. 
+    
  
 
 def make_exclamation(sentence_list):
